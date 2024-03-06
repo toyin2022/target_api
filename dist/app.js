2009f122108341ab02db781f782485422e5a8e46
toyin2022 <shevioni@yahoo.com>
2024-03-06 16:37:42 +0100

    package update

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const db_1 = __importDefault(require("./model/db"));
@@ -16,24 +18,29 @@ app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 dotenv_1.default.config();
 (0, db_1.default)(process.env.MONGO_URI);
-app.use((0, cors_1.default)({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  (0, cors_1.default)({ origin: "http://localhost:5173", credentials: true })
+);
 // app.use(MongoError());
 app.use((err, req, res, next) => {
-    if (err instanceof mongodb_1.MongoServerError && err.code === 11000) {
-        return res.status(400).json({ message: "User already exists" });
-    }
-    res.status(500).json({ message: "Internal server error" });
+  if (err instanceof mongodb_1.MongoServerError && err.code === 11000) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+  res.status(500).json({ message: "Internal server error" });
 });
 app.use((err, req, res, next) => {
-    // Send an appropriate error response based on the error type
-    res.status(500).json({ message: "Internal server error" });
+  // Send an appropriate error response based on the error type
+  res.status(500).json({ message: "Internal server error" });
 });
 //          ROUTES
 app.use("/auth", user_1.default);
 app.use("/tasks", task_1.default);
 app.get("/", (req, res) => {
-    res.send("hello world!");
+  res.send("hello world!");
+});
+app.get("/test", (req, res) => {
+  res.send("hello test route!");
 });
 app.listen(3000, () => {
-    console.log("app listening on  3000");
+  console.log("app listening on  3000");
 });
