e89a986367e816fbd02f09b009f4f6c6524a0a6a
toyin2022 <104086364+toyin2022@users.noreply.github.com>
2024-03-06 17:29:06 +0100

    Update cors

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(cookieParser());
 dotenv.config();
 connection(process.env.MONGO_URI as string);
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://target-ten.vercel.app", credentials: true }));
 // app.use(MongoError());
 app.use(
   (
